50f4de3cd7cf8b5e4e9a6d7a882b5e93973d64d6
Kirill Gribov <kirill@gribov.dev>
2024-03-06 15:58:53 +0000

    Change razz on razz-team

--- build.gradle.kts
+++ build.gradle.kts
@@ -27,7 +27,7 @@ allprojects {
 subprojects {
   plugins.withType(io.bkbn.sourdough.gradle.library.jvm.LibraryJvmPlugin::class) {
     extensions.configure(io.bkbn.sourdough.gradle.library.jvm.LibraryJvmExtension::class) {
-      githubOrg.set("razz")
+      githubOrg.set("razz-team")
       githubRepo.set("kompendium")
       licenseName.set("MIT License")
       licenseUrl.set("https://mit-license.org")
