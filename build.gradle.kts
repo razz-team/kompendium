68036e5ba9313d103260bd936476812c8fcdf1ee
Kirill Gribov <kirill@gribov.dev>
2024-03-06 14:49:24 +0000

    Change github org to publish packages

--- build.gradle.kts
+++ build.gradle.kts
@@ -27,7 +27,7 @@ allprojects {
 subprojects {
   plugins.withType(io.bkbn.sourdough.gradle.library.jvm.LibraryJvmPlugin::class) {
     extensions.configure(io.bkbn.sourdough.gradle.library.jvm.LibraryJvmExtension::class) {
-      githubOrg.set("bkbnio")
+      githubOrg.set("razz")
       githubRepo.set("kompendium")
       licenseName.set("MIT License")
       licenseUrl.set("https://mit-license.org")
